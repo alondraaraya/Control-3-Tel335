32708dd03ee70238259e5f24583f52a24c9e4745
alaraya <alondra.araya@usm.cl>
2022-05-14 17:29:59 -0400

    arreglos carpetas

---
+++ Frontend/src/reducers/carritoReducer.js
@@ -0,0 +1,50 @@
+import {
+  ADD_TO_CART,
+
+} from "../Types";
+
+export const initialState = {
+  cardInfo: [
+    {
+      image: "https://media.istockphoto.com/photos/kefir-milk-or-turkish-ayran-drink-are-poured-into-a-glass-cup-from-a-picture-id1198789194",
+      id : "1",
+      name: "Leche",
+      marca: "Soprole",
+      precio: "990"
+    },
+
+  ],
+  cart: [],
+};
+
+export function shoppingReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TO_CART: {
+      console.log("agregado")
+      let newItem = state.cardInfo.find(
+        (product) => product.id === action.payload
+      );
+      console.log("newItem");
+
+      let itemInCart = state.cart.find((item) => item.id === newItem.id);
+
+      return itemInCart
+        ? {
+            ...state,
+            cart: state.cart.map((item) =>
+              item.id === newItem.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            ),
+          }
+        : {
+            ...state,
+            cart: [...state.cart, { ...newItem, quantity: 1 }],
+          };
+      
+    }
+
+    default:
+      return state;
+  }
+}
